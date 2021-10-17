d3cddf49de59518d51256ce3623c16c0e59d478d
Anas Nadeem <57933598+AnasNadeem@users.noreply.github.com>
2021-10-17 06:58:26 +0000

    Added some React components and Routers

--- music_room_frontend/urls.py
+++ music_room_frontend/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path  
 from music_room_frontend.views import index
 urlpatterns = [
-  path('', index, name='index')
+  path('', index, name='index'),
+  path('join', index),
+  path('create', index),
 ]
