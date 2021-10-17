22e4c4d7dd56d6cec6447f101b553a29a1a9b9f6
Anas Nadeem <57933598+AnasNadeem@users.noreply.github.com>
2021-10-17 08:12:51 +0000

    Added createroomserializer and some more

--- music_room_app/urls.py
+++ music_room_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from music_room_app.views import RoomView
+from music_room_app.views import RoomView,CreateRoomView
 
 urlpatterns = [
-  path('', RoomView.as_view(), name='home')
+  path('', RoomView.as_view(), name='home'),
+  path('create-room', CreateRoomView.as_view(), name='create'),
 ]
